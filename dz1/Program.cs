40a62728be4d6581193b7730cd294a068b1a2701
vjlion <89218741998@mail.ru>
2023-01-16 22:19:47 +0300

    dz1

---
+++ dz1/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/
+int getUserDate(string message)
+{
+    Console.ForegroundColor = ConsoleColor.DarkCyan;
+    Console.WriteLine(message);
+    Console.ResetColor();
+    int UserData = int.Parse(Console.ReadLine()!);
+    return UserData;
+}
+int getExtOfRange(int endA, int endB)
+{
+    int ext = 1;
+    for (int i = 1; i <= endB; i++)
+    {
+        ext = ext * endA;
+    }
+    return ext;
+}
+int endA = getUserDate("Введите число A");
+int endB = getUserDate("Введите число В");
+int ext = getExtOfRange(endA,endB);
+Console.WriteLine(ext);
