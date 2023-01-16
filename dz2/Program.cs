96c22bdc14737f3a4e4fa808d6fba173828b019c
vjlion <89218741998@mail.ru>
2023-01-16 23:29:28 +0300

    dz2

--- dz2/Program.cs
+++ dz2/Program.cs
@@ -4,3 +4,25 @@
 82 -> 10
 9012 -> 12
 */
+int getUserDate(string message)
+{
+    Console.ForegroundColor = ConsoleColor.DarkCyan;
+    Console.WriteLine(message);
+    Console.ResetColor();
+    int UserData = int.Parse(Console.ReadLine()!);
+    return UserData;
+}
+int summa(int a)
+{
+    int sum = 0;
+    for (int i = 0; i <= a; i++)
+    {
+        sum += a % 10;
+        a = a / 10;
+    }
+    return sum;
+}
+
+int a = getUserDate("Введите число ");
+int sum = summa(a);
+Console.WriteLine(sum);
