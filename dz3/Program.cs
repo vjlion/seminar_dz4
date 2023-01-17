226955c3cf5fd2ed01354e162215a2ed25ef84f3
vjlion <89218741998@mail.ru>
2023-01-17 21:16:42 +0300

    dz3

--- dz3/Program.cs
+++ dz3/Program.cs
@@ -3,3 +3,15 @@
 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 6, 1, 33 -> [6, 1, 33]
 */
+Console.Clear();
+int numMassive = 8;
+int[] SetArray(int length)
+{
+    int[] arr = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        arr[i] = new Random().Next(1, 100);
+    }
+    return arr;
+}
+Console.WriteLine('[' + string.Join(", ", SetArray(numMassive)) + ']');
